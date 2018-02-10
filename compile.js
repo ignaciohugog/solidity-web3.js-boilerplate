a7e47fa91091b099b4a3fd7b566641740e9187a5
Narek <promentol@gmail.com>
2018-02-10 23:43:11 +0400

    little fix in compile

--- compile.js
+++ compile.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const lotteryPath = path.resolve(__dirname, 'contracts', 'Contract.sol');
+const contractPath = path.resolve(__dirname, 'contracts', 'Contract.sol');
 const source = fs.readFileSync(contractPath, 'utf8');
 
 module.exports = solc.compile(source, 1).contracts[':Contract'];
